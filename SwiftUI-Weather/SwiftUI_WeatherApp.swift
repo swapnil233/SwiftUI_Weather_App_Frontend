89c994379814c853db7d6314eaeadf53b569112b
Hasan Iqbal <swapniliqbal@gmail.com>
2021-08-22 01:14:01 -0400

    UI Done

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Hasan Iqbal on 2021-08-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
